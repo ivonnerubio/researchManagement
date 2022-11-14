53405cd102c85ff799da9cc1d8c430a4c85c5923
Ivonne <irubio081@outlook.com>
2022-11-14 17:18:30 -0600

    adding spring boot starter files

---
+++ src/main/java/com/irubio/researchManagement/ResearchManagementApplication.java
@@ -0,0 +1,13 @@
+package com.irubio.researchManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irubio/researchManagement/ResearchManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irubio.researchManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
